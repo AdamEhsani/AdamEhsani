bc0a71beb796548a79e9ab73e24905fc1e6542df
AdamEhsani <mahdiehsanirad@gmail.com>
2023-08-30 10:53:05 +0200

    Added prework files to repository

---
+++ AddUserOrderDirect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AddUserOrderDirect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
